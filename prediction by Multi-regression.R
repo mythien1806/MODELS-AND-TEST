ecda1a4947306310ed23f0e99d8efb369c8b08c7
Thien Nguyen <92000467+mythien1806@users.noreply.github.com>
2021-10-25 05:06:35 -0400

    Add files via upload

---
+++ prediction by Multi-regression.R
@@ -0,0 +1,45 @@
+#' ---
+#' title: "Multiple Regression"
+#' author: "Nhat My Thien Nguyen"
+#' date: "4/3/2021"
+#' output: html_document
+#' ---
+#' 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
